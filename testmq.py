ca23b5f88736f4ee0c11fd8b450a8e375899236f
Quang <thaihongquang1202@gmail.com>
2024-04-03 23:40:08 +0700

    add MQ test

---
+++ testmq.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import websocket
+
+MQ_PIN = 19
+
+socket = websocket.WebSocket
+socket.connect("loaclhost:3000")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MQ_PIN, GPIO.IN)
+
+def read():
+    try:
+        while True:
+            alcohol = GPIO.input(MQ_PIN)
+            print("nong do con",alcohol)
+            message=("nong do con", alcohol)
+            socket.send (str(message))
+            time.sleep = 1
+    except KeyboardInterrupt:
+        GPIO.cleanup
+if __name__ == "__main__":
+    read
+
+
